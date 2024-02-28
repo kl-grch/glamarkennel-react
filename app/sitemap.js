0d3b2302fdf4deaa966b078118f99de0367ae982
Nikolay Gerasimov <kolyagerich@gmail.com>
2024-02-28 12:49:40 +0300

    add file sitemap.js

---
+++ app/sitemap.js
@@ -0,0 +1,70 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://www.glamarkennel.com/",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: "https://www.glamarkennel.com/dogs",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.glamarkennel.com/schnauzer",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.5,
+    },
+    {
+      url: "https://www.glamarkennel.com/australian_terrier",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.5,
+    },
+    {
+      url: "https://www.glamarkennel.com/puppies",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.5,
+    },
+    {
+      url: "https://www.glamarkennel.com/litters/afterRegistration",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.5,
+    },
+    {
+      url: "https://www.glamarkennel.com/litters/beforeRegistration",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.5,
+    },
+    {
+      url: "https://www.glamarkennel.com/results/",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.5,
+    },
+    {
+      url: "https://www.glamarkennel.com/articles",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.5,
+    },
+    {
+      url: "https://www.glamarkennel.com/about",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.5,
+    },
+    {
+      url: "https://www.glamarkennel.com/contacts",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.5,
+    },
+  ];
+}
